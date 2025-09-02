4bbc3a1821b77066c16392da3f1a5fb0bd730824
BenediktBor <benedikt.bornemann@gmail.com>
2025-09-02 15:41:30 +0200

    upd: ProseH2 margin

--- app/components/content/ProseH2.vue
+++ app/components/content/ProseH2.vue
@@ -1,6 +1,6 @@
 <template>
 	<h2
-		class="text-2xl sm:text-3xl lg:text-4xl text-pretty tracking-tight font-bold text-highlighted text-start"
+		class="mt-12 mb-2 text-2xl sm:text-3xl lg:text-4xl text-pretty tracking-tight font-bold text-highlighted text-start"
 	>
 		<slot />
 	</h2>
